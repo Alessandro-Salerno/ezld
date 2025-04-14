7ab548ff14e57b3aa3312d1df7c4505fc5e81699
Alessandro-Salerno <oxygen05@outlook.it>
2025-04-14 20:10:58 +0200

    better workflow?

--- .github/workflows/make-archives.sh
+++ .github/workflows/make-archives.sh
@@ -3,10 +3,6 @@ git config --local user.name "github-actions[bot]"
 git checkout --orphan latest-build
 
 mkdir $1
-cp ./bin/ezld $1/ezld
-cp -r ./bin/plugins $1/plugins
-cp ./bin/plugin-sdk.o $1/plugin-sdk.o
-
 mkdir ezld
 cp ./bin/ezld ./ezld/ezld
 tar -cvzf ./$1/ezld-$1.tar.gz ./ezld
